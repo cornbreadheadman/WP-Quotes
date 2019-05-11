e75258795eaf33f95611485ea7fa7ef374120bf0
Jason Dobson <jasonldobson@gmail.com>
2019-05-11 19:53:20 -0400

    Update class-wp-quotes-i18n.php

--- wp-quotes/includes/class-wp-quotes-i18n.php
+++ wp-quotes/includes/class-wp-quotes-i18n.php
@@ -7,7 +7,7 @@
  * so that it is ready for translation.
  *
  * @link       https://cornbread.me
- * @since      1.0.0
+ * @since      0.0.1
  *
  * @package    Wp_Quotes
  * @subpackage Wp_Quotes/includes
@@ -19,7 +19,7 @@
  * Loads and defines the internationalization files for this plugin
  * so that it is ready for translation.
  *
- * @since      1.0.0
+ * @since      0.0.1
  * @package    Wp_Quotes
  * @subpackage Wp_Quotes/includes
  * @author     Cornbread <cornbread@cornbread.me>
@@ -30,7 +30,7 @@ class Wp_Quotes_i18n {
 	/**
 	 * Load the plugin text domain for translation.
 	 *
-	 * @since    1.0.0
+	 * @since    0.0.1
 	 */
 	public function load_plugin_textdomain() {
 
