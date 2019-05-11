d4e6ddd48c54b1a892ffa272e24587cc2cc315bf
Jason Dobson <jasonldobson@gmail.com>
2019-05-11 18:05:38 -0400

    uploaded boilerplate
    boilerplate files to start project

---
+++ wp-quotes/includes/class-wp-quotes-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://cornbread.me
+ * @since      1.0.0
+ *
+ * @package    Wp_Quotes
+ * @subpackage Wp_Quotes/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Quotes
+ * @subpackage Wp_Quotes/includes
+ * @author     Cornbread <cornbread@cornbread.me>
+ */
+class Wp_Quotes_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
