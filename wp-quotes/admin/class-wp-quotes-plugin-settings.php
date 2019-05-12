d813b0b07b8902a95c2943f96e910179658e03da
Jason Dobson <jasonldobson@gmail.com>
2019-05-11 22:05:52 -0400

    Create class-wp-quotes-plugin-settings.php

---
+++ wp-quotes/admin/class-wp-quotes-plugin-settings.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The settings of the plugin.
+ *
+ * @link              https://cornbread.me
+ * @since             0.0.1
+ * @package           Wp_Quotes
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Random Quote
+ * Plugin URI:        https://github.com/cornbreadheadman/WP-Quotes
+ * Description:       This plugin displays a random quote from a mySQL server.
+ * Version:           0.0.1
+ * Author:            Cornbread
+ * Author URI:        https://cornbread.me
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-quotes
+ * Domain Path:       /languages
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Class WordPress_Plugin_Template_Settings
+ *
+ */
+ 
